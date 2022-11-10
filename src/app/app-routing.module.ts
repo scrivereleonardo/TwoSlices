4fcd47d25fcfa483948cfc70ffbd80d222682d06
scrivereleonardo <studente.leonardo.scriver@gmail.com>
2022-11-10 22:39:07 +0100

    First Commit Two slices

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './Pages/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { component: HomepageComponent, path: "" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
