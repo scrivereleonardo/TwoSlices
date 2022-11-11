33b9f50caab41f6e94e39deaacbe870bca760510
scrivereleonardo <studente.leonardo.scriver@gmail.com>
2022-11-12 00:55:36 +0100

    Tante immagini nuove

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'twoSlices';
+  removeLoading:boolean=false
+  ngOnInit(){
+    setTimeout(() => {
+      this.removeLoading=true
+    }, 5000);
+  }
+  
 }
