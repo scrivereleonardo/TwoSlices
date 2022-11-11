ce50f10bd114bd9703357b3b62439a229317a51a
scrivereleonardo <studente.leonardo.scriver@gmail.com>
2022-11-11 19:13:51 +0100

    Nuovi Colori e piccole aggiunte varie

--- src/app/Pages/header/header.component.ts
+++ src/app/Pages/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Aos from 'aos';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,8 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    Aos.init()
+
   }
 
 }
