d4d08135aaa04b79706455d843289a4890b5d3d0
scrivereleonardo <studente.leonardo.scriver@gmail.com>
2022-11-12 01:25:34 +0100

    Cambio palette colori

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,10 +6,14 @@ module.exports = {
   theme: {
     extend: {
       colors:{
-        "primary":"#05445E",
-        "secondary":"#189AB4",
-        "tertiary":"#75E6DA",
-        "quaternary":"#D4F1F4"
+        // "primary":"#05445E",
+        // "secondary":"#189AB4",
+        // "tertiary":"#75E6DA",
+        // "quaternary":"#D4F1F4",
+        "primary":"#003060",
+        "secondary":"#055C9D",
+        "tertiary":"#0E86D4",
+        "quaternary":"#68BBE3"
 
       }
     },
